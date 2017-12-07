eee3cdf80cfad631fcb9c4f19e01a445f0a0288b
DESKTOP-ND0O0CJ\PC-HOME1 <jefferson.llumiquinga@epn.edu.ec>
2017-12-06 22:15:59 -0500

    commit_3

--- LecturaTxt/LecturaTxt.py
+++ LecturaTxt/LecturaTxt.py
@@ -1,6 +1,25 @@
+def creartxt():
+    archi=open('lectura.txt','w')
+    archi.close()
 
+def grabartxt():
+    archi=open('lectura.txt','a')
+    archi.write(input("Frase"))
+    archi.close()
 
 def leertxt():
+    archi=open('lectura.txt','r')
+    linea=archi.readline()
+    datos=linea
+    while linea!="":
+     # print(linea)  # se puede comentar esta linea para que no aparesca todo el texto
+      linea=archi.readline()
+
+    archi.close()
+    nuevo = datos.split(" ")
+    print("Existen: "+str(len(nuevo))+" palabras")
+
+def leertxt2():
     archi=open('cuento.txt','r')
     linea=archi.readline()
     datos=linea
@@ -12,9 +31,11 @@ def leertxt():
     nuevo = datos.split(" ")
     print("Existen: "+str(len(nuevo))+" palabras")
 
-    # for p in texto:
-    #     print(p, len(p))
-
+creartxt()
+grabartxt()
 leertxt()
+leertxt2()
 
 print("Nombre: Jefferson Omar Diaz")
+print("Nombre: Daniel Flores")
+print("Nombre: Jefferson Llumiquinga")
